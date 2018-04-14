dfa8dd0bf3347f96b659f6d72ebb72088134310b
Edwards80 <Scott@edwards80.co.uk>
2018-04-14 13:38:07 +0100

    Edited pattern card styling

--- src/PatternCard.js
+++ src/PatternCard.js
@@ -3,22 +3,21 @@ import React, { Component } from 'react';
 class PatternCard extends Component {
   render() {
     return (
-      <div class="card">
-        <div class="card-image">
-          <figure class="image is-4by3">
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
             <img src="https://bulma.io/images/placeholders/480x480.png" alt="Pattern Preview" />
           </figure>
         </div>
-        <div class="card-content">
-          <div class="media-content">
-            <p class="title is-4">Pattern Title</p>
-            <p class="subtitle is-6">Style</p>
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">Pattern Title</p>
+            <a href="Link to styles"><div className="tag is-primary">Style Tag</div></a>
           </div>
         </div>
 
-        <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris.
+        <div className="content">
+          Here is a brief description of the knitting pattern
           <br />
           <a href="link to pattern">LINK TO PATTERN</a>
         </div>
