a71bf2f2cf2d9342ef2c331816e95c293a36e731
Edwards80 <Scott@edwards80.co.uk>
2018-04-21 21:04:26 +0100

    Removed 'function wrapped in a function'

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -12,33 +12,29 @@ class Instructions extends Component {
 }
 
 const parseRow = (row, rowNum) => {
+  let prevStitch = '';
+  let count = 1;
 
-  const parseRow = (row, rowNum) => {
-    let prevStitch = '';
-    let count = 1;
-
-    return row.reduce((inst, stitch, i) => {
-      if (i === 0) {
-        prevStitch = stitch.stitchType;
-      } else if (i === row.length - 1) {
-        if (stitch.stitchType === prevStitch) {
-          inst += `${stitch.stitchType}${count + 1}`;
-        } else {
-          inst += `${prevStitch}${count}, ${stitch.stitchType}${1}`;
-        }
-      } else if (stitch.stitchType === prevStitch) {
-        count++;
+  return row.reduce((inst, stitch, i) => {
+    if (i === 0) {
+      prevStitch = stitch.stitchType;
+    } else if (i === row.length - 1) {
+      if (stitch.stitchType === prevStitch) {
+        inst += `${stitch.stitchType}${count + 1}`;
       } else {
-        inst += `${prevStitch}${count}, `;
-        prevStitch = stitch.stitchType;
-        count = 1;
+        inst += `${prevStitch}${count}, ${stitch.stitchType}${1}`;
       }
+    } else if (stitch.stitchType === prevStitch) {
+      count++;
+    } else {
+      inst += `${prevStitch}${count}, `;
+      prevStitch = stitch.stitchType;
+      count = 1;
+    }
 
-      return inst;
-    }, '');
-  };
-
-  return <p>{`Row ${rowNum}: `}{parseRow(row)}</p>;
+    return inst;
+  }, '');
 };
+
 
 export default Instructions;
